bbc8cb938681f6953b3e3a96490a883d06d0cf0a
lekkyj <lekanjayeola0@gmail.com>
2025-01-27 02:47:51 -0800

    web developer

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // darkmode: 'media',
+  darkMode: 'class',
+  content: ["*"],
+  theme: {
+    extend: {
+      screens:{
+        sm: '450px',
+      }
+    },
+  },
+  plugins: [],
+}
+
